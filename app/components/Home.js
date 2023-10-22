b55dcaa240fcaf83330203536ced9c91de934a38
chris b <christopherbrowning@Christophers-MacBook-Pro.local>
2023-10-22 18:51:30 +0200

    setup navgiation stack / custoom category

---
+++ app/components/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button, View, Text } from "react-native";
+import { StyleSheet } from "react-native";
+
+function Home({ navigation }) {
+  const handlePress = () => {
+    navigation.navigate("CustomCategory");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Customise a category</Text>
+      <Button title="Click here!" onPress={handlePress} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default Home;
