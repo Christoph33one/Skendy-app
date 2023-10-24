ab1b671efd8cdd0f7b1b92b45baa7fa0493f78c0
chris b <christopherbrowning@Christophers-MacBook-Pro.local>
2023-10-24 19:00:41 +0200

    built custome icon componet

--- components/IconPicker.js
+++ components/IconPicker.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
-import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  Button,
+} from "react-native";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 function IconPicker(props) {
   const [selectedIcon, setSelectedIcon] = useState(null);
-  const [catergoryName, setCategoryName] = useState("");
-  const [categories, setCategoris] = useState([]);
-
+  const [categoryName, setCategoryName] = useState("");
+  const [categories, setCategories] = useState([]);
 
   const iconList = [
     { icon: "book", title: "Tax" },
@@ -23,23 +29,55 @@ function IconPicker(props) {
   ];
 
   const handleIconSelected = (icon) => {
-    selectedIcon(icon);
-  }
+    setSelectedIcon(icon);
+  };
 
+  const handleCategoryNameChange = (text) => {
+    setCategoryName;
+  };
+
+  const handleAddCategory = () => {
+    if (categoryName.trim() !== "" && selectedIcon) {
+      setCategories((prevCategories) => [
+        ...prevCategories,
+        { name: categoryName, icon: selectedIcon },
+      ]);
+      setCategoryName("");
+      selectedIcon(null);
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Text>Custom Category</Text>
-      <View 
-      {iconList.map((item, index) => (
-        <TouchableOpacity 
-        key={index}
-        title={item.title}
-        onPress={() => handleIconSelected(item.icon)}
+      <View style={styles.inputContainer}>
+        <MaterialCommunityIcons name={selectedIcon} size={40} color="#45C786" />
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Add Category Name"
+          value={setCategoryName}
+          onChangeText={handleCategoryNameChange}
         />
-      ))}
-      />
-      <MaterialCommunityIcons />
+      </View>
+
+      <View style={styles.iconContainer}>
+        {iconList.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            style={styles.icon}
+            onPress={() => handleIconSelected(item.icon)}
+          >
+            <Text>{item.title}</Text>
+            <MaterialCommunityIcons
+              name={item.icon}
+              size={50}
+              padding={15}
+              color="#45C786"
+            />
+          </TouchableOpacity>
+        ))}
+      </View>
+      <Button title="Create Category" onPress={handleAddCategory} />
     </View>
   );
 }
@@ -49,6 +87,29 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     paddingTop: "30%",
+    backgroundColor: "#fff",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  TextInput: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 2,
+    borderRadius: 5,
+    padding: 10,
+  },
+  iconContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    margin: 10,
+  },
+  icon: {
+    padding: 10,
+    alignItems: "center",
   },
 });
 
